d467cbfc35af96da098a2240ebea4b4803e993b6
Andrey Bragin <andrey.bragin@jetbrains.com>
2025-09-03 00:29:50 +0200

    [a2a] Add HTTP JSON-RPC client test

--- a2a/a2a-transport/a2a-transport-client-jsonrpc-http/build.gradle.kts
+++ a2a/a2a-transport/a2a-transport-client-jsonrpc-http/build.gradle.kts
@@ -30,6 +30,8 @@ kotlin {
         commonTest {
             dependencies {
                 implementation(kotlin("test"))
+                implementation(libs.kotlinx.coroutines.test)
+                implementation(libs.ktor.client.mock)
             }
         }
 
